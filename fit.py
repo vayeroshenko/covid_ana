3ea6e2152ae5c94880cb882c6fd8d4b67f736cbe
Vsevolod Yeroshenko <vayeroshenko@gmail.com>
2020-04-04 17:57:32 +0300

    cleaned the script

--- fit.py
+++ fit.py
@@ -1,59 +1,39 @@
-from math import sqrt
-
-from ROOT import TGraphErrors, TF1, TMath, TCanvas, TPad, TH1D, TAttFill
-from ROOT import kBlue, kRed, gStyle, gROOT
-
-from tools import *
+from plotter import Plotter
+from plotter_sir import Plotter_SIR
 
 
 
-# country = "wo China"
-# country = "China"
-# country = "Spain"
-country = "Ukraine"
-# country = "Belarus"
-# country = "Italy"
-# country = "France"
-# country = "US"
-# country = "India"
+############### To draw number of cases #################
 
-# hist_conf, hist_dead, hist_rec, legend = make_SIR(country)
-hist_conf, hist_dead, hist_rec, legend = make_def(country)
+plotter = Plotter("Ukraine",
+		draw_seq = ("confirmed", "deaths", "recovered"),
+		fit_start = "03/30/20",
+		fit_end = "04/03/20",
+		draw_fit = True,
+		derivative = 0
+		)
 
-
-gStyle.SetOptStat(0)
-gROOT.ForceStyle()
-
-
-# trans_rate = 0.01132018 
-# reco_rate = 0.0053761 
+# plotter = Plotter("wo China",
+# 		draw_seq = ("confirmed", "recovered", "deaths"),
+# 		fit_start = "03/30/20",
+# 		fit_end = "04/03/20",
+# 		draw_fit = True,
+# 		derivative = 0
+# 		)
 
 
-# trans_rate, reco_rate = fit_SIR("China", ("02/22/20", "03/31/20"))
-# trans_rate, reco_rate = fit_SIR("Italy", ("02/29/20", "04/01/20"))
-# hist_conf, hist_dead, hist_rec, legend = draw_SIR(trans_rate, reco_rate, ("02/22/20", "03/31/20"))
+plotter.draw()
 
-# print trans_rate / reco_rate
-
-c = TCanvas("c", "c", 1920, 1080)
-
-hist_conf.Draw("MIN0 HIST E1")
-hist_rec.Draw("HIST E1 SAME")
-hist_dead.Draw("HIST E1 SAME")
+###########################################################
 
 
-# hist_dead.Draw("MIN0 HIST E1")
-# hist_rec.Draw("HIST E1 SAME")
-# hist_rec.Draw("MIN0 HIST E1")
-# hist_dead.Draw("HIST E1 SAME")
+####################### To draw SIR #######################
 
+# plotter = Plotter_SIR("Ukraine",
+# 		draw_seq = ("infected", "recovered"),
+# 		to_fit = False
+# 		)
 
-func_conf = fit_hist(hist_conf, "03/30/20", "04/03/20")
-print_slope(func_conf)
-func_conf.Draw("same")
+# plotter.draw()
 
-legend.Draw()
-
-# func_conf.Draw("SAME")
-
-
+###########################################################
